680ba586d30087b5b3076be6736c1ae815ec7c03
Marlon Graichy <mgraichy@gmail.com>
2024-12-23 17:54:55 -0500

    add LICENSE.md, README.md
    * add Carbon\CarbonImmutable to database/seeders/VideoSeeder.php

--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -34,34 +35,31 @@ class VideoSeeder extends Seeder
                 'user_id' => $user->id,
                 'client_id' => 1,
                 'title' => 'The Birthday Bot',
-                // 'video' => 'https://www.youtube.com/embed/nNjaXpo7RP0?si=DJa4HMNH6tEu5TrJ',
                 'video' => 'bday-bot-540p.mp4',
                 'views' => '930K',
                 'comment' => fake()->paragraphs(3, true),
-                'created_at' => '2024-03-16 17:37:05',
-                'updated_at' => '2024-03-16 17:37:05',
+                'created_at' => CarbonImmutable::now(),
+                'updated_at' => CarbonImmutable::now(),
             ],
             [
                 'user_id' => $user->id,
                 'client_id' => 1,
                 'title' => 'A Small Town',
-                // 'video' => 'https://www.youtube.com/embed/RzpQjPI0RfM?si=QprCAVHruSMA5-me',
                 'video' => 'town-540p.mp4',
                 'views' => '32K',
                 'comment' => fake()->paragraphs(3, true),
-                'created_at' => '2024-03-16 17:37:05',
-                'updated_at' => '2024-03-16 17:37:05',
+                'created_at' => CarbonImmutable::now(),
+                'updated_at' => CarbonImmutable::now(),
             ],
             [
                 'user_id' => $user->id,
                 'client_id' => 1,
                 'title' => 'Traffic',
-                // 'video' => 'https://www.youtube.com/embed/T6Y3OcWMUdI?si=y9_RZH9V1TDXsAJ3',
                 'video' => 'traffic-540p.mp4',
                 'views' => '23K',
                 'comment' => fake()->paragraphs(3, true),
-                'created_at' => '2024-03-16 17:37:05',
-                'updated_at' => '2024-03-16 17:37:05',
+                'created_at' => CarbonImmutable::now(),
+                'updated_at' => CarbonImmutable::now(),
             ],
         ];
 
